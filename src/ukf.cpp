9076d08368761918b27721800d999b464cb5e9bc
Pascal Irminger <pascal.irminger@gmail.com>
2018-02-11 10:37:30 +0100

    Process noise

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -33,10 +33,10 @@ UKF::UKF() {
         0, 0, 0, 0, 1;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 1.5;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.5;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
